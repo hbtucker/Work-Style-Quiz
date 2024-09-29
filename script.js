eb95716b721db78585774b93230dd6c449ca8d80
hbtucker <132301974+hbtucker@users.noreply.github.com>
2024-09-29 17:10:11 +1300

    Update script.js

--- script.js
+++ script.js
@@ -224,24 +224,20 @@ function generatePDF() {
         doc.text("Work Styles Overview", 20, yPos);
         yPos += 10;
         doc.setFont('Poppins', 'normal');
-        doc.setFontSize(11);
+        doc.setFontSize(12);
         const additionalText = [
-        "This quiz assessed your work style preferences across 4 different categories:",
-        "Collaborative, Traditional, Innovative, and Remote.",
-        "The scores reflect your tendencies towards each work style,",
-        "though they may change depending on your work environment.",
+        "This quiz assessed your work style preferences across 4 different categories: Collaborative, Traditional, Innovative, and Remote.",
+        "The scores reflect your tendencies towards each work style, though they may change depending on your work environment.",
         "You can use these insights to understand and optimize your work environment.",
-        "Start by sharing the quiz with your team and share the results.",
-        "Next, discuss how to best interact with each other.",
-        "You can use physical whiteboards or digital tools like Miro to jot down ideas",
-        "and decide how you can work effectively together as a team.",
+        "Start by sharing the quiz with your team and share the results.Next, discuss how to best interact with each other.",
+        "You can use physical whiteboards or digital tools like Miro to jot down ideas and decide how you can work effectively together as a team.",
         "Reach out to me at hello@culture-coach.org if you want some help getting started.",
         "Your profile is visualized in the polar chart on the next page.",
 
         ];
         additionalText.forEach(text => {
-            doc.text(text, 20, yPos);
-            yPos += 10;
+          doc.text(text, 20, yPos, { maxWidth: 170 });
+          yPos += doc.getTextDimensions(text, { maxWidth: 170 }).h + 2;
         });
 
         html2canvas(document.getElementById("polarChart")).then(canvas => {
